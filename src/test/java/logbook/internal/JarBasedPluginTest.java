13e10fdf8e0f81f0b98603e23a7f5ba314239e9e
sanaehirotaka <admin@sanaechan.net>
2015-11-09 17:56:55 +0900

    jarファイルがgitignoreに含まれるのでテスト用jarファイルの拡張子を変える

--- src/test/java/logbook/internal/JarBasedPluginTest.java
+++ src/test/java/logbook/internal/JarBasedPluginTest.java
@@ -21,7 +21,7 @@ public class JarBasedPluginTest {
      */
     @Test
     public void test1() throws IOException {
-        Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case1.jar");
+        Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case1.jar.bin");
         new JarBasedPlugin(p);
     }
 
@@ -40,7 +40,7 @@ public class JarBasedPluginTest {
      */
     @Test
     public void testGetURL() throws IOException {
-        Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case1.jar");
+        Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case1.jar.bin");
         URL expected = p.toUri().toURL();
         URL actual = new JarBasedPlugin(p).getURL();
         assertEquals(expected, actual);
@@ -53,7 +53,7 @@ public class JarBasedPluginTest {
     @Test
     public void testGetServices() throws IOException {
         {
-            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case1.jar");
+            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case1.jar.bin");
             JarBasedPlugin plugin = new JarBasedPlugin(p);
             List<String> expected = Arrays.asList("logbook.test.TestSpi1", "logbook.test.TestSpi2");
             List<String> actual = new ArrayList<>(plugin.getServices());
@@ -61,7 +61,7 @@ public class JarBasedPluginTest {
             assertEquals(expected, actual);
         }
         {
-            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case2.jar");
+            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case2.jar.bin");
             JarBasedPlugin plugin = new JarBasedPlugin(p);
             List<String> expected = Collections.emptyList();
             List<String> actual = plugin.getServices();
@@ -76,28 +76,28 @@ public class JarBasedPluginTest {
     @Test
     public void testGetName() throws IOException {
         {
-            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case1.jar");
+            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case1.jar.bin");
             JarBasedPlugin plugin = new JarBasedPlugin(p);
             String expected = "test-plugin(Bundle-Name)";
             String actual = plugin.getName();
             assertEquals(expected, actual);
         }
         {
-            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case2.jar");
+            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case2.jar.bin");
             JarBasedPlugin plugin = new JarBasedPlugin(p);
             String expected = "test-plugin(Implementation-Title)";
             String actual = plugin.getName();
             assertEquals(expected, actual);
         }
         {
-            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case3.jar");
+            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case3.jar.bin");
             JarBasedPlugin plugin = new JarBasedPlugin(p);
             String expected = "test-plugin(Specification-Title)";
             String actual = plugin.getName();
             assertEquals(expected, actual);
         }
         {
-            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case4.jar");
+            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case4.jar.bin");
             JarBasedPlugin plugin = new JarBasedPlugin(p);
             String expected = "";
             String actual = plugin.getName();
@@ -112,28 +112,28 @@ public class JarBasedPluginTest {
     @Test
     public void testGetVendor() throws IOException {
         {
-            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case1.jar");
+            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case1.jar.bin");
             JarBasedPlugin plugin = new JarBasedPlugin(p);
             String expected = "test-plugin(Bundle-Vendor)";
             String actual = plugin.getVendor();
             assertEquals(expected, actual);
         }
         {
-            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case2.jar");
+            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case2.jar.bin");
             JarBasedPlugin plugin = new JarBasedPlugin(p);
             String expected = "test-plugin(Implementation-Vendor)";
             String actual = plugin.getVendor();
             assertEquals(expected, actual);
         }
         {
-            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case3.jar");
+            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case3.jar.bin");
             JarBasedPlugin plugin = new JarBasedPlugin(p);
             String expected = "test-plugin(Specification-Vendor)";
             String actual = plugin.getVendor();
             assertEquals(expected, actual);
         }
         {
-            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case4.jar");
+            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case4.jar.bin");
             JarBasedPlugin plugin = new JarBasedPlugin(p);
             String expected = "";
             String actual = plugin.getVendor();
@@ -148,28 +148,28 @@ public class JarBasedPluginTest {
     @Test
     public void testGetVersion() throws IOException {
         {
-            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case1.jar");
+            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case1.jar.bin");
             JarBasedPlugin plugin = new JarBasedPlugin(p);
             String expected = "test-plugin(Bundle-Version)";
             String actual = plugin.getVersion();
             assertEquals(expected, actual);
         }
         {
-            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case2.jar");
+            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case2.jar.bin");
             JarBasedPlugin plugin = new JarBasedPlugin(p);
             String expected = "test-plugin(Implementation-Version)";
             String actual = plugin.getVersion();
             assertEquals(expected, actual);
         }
         {
-            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case3.jar");
+            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case3.jar.bin");
             JarBasedPlugin plugin = new JarBasedPlugin(p);
             String expected = "test-plugin(Specification-Version)";
             String actual = plugin.getVersion();
             assertEquals(expected, actual);
         }
         {
-            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case4.jar");
+            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case4.jar.bin");
             JarBasedPlugin plugin = new JarBasedPlugin(p);
             String expected = "";
             String actual = plugin.getVersion();
@@ -184,14 +184,14 @@ public class JarBasedPluginTest {
     @Test
     public void testGetLicense() throws IOException {
         {
-            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case1.jar");
+            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case1.jar.bin");
             JarBasedPlugin plugin = new JarBasedPlugin(p);
             String expected = "test-plugin(Bundle-License)";
             String actual = plugin.getLicense();
             assertEquals(expected, actual);
         }
         {
-            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case4.jar");
+            Path p = Paths.get("./src/test/resources/logbook/internal/plugin-test-case4.jar.bin");
             JarBasedPlugin plugin = new JarBasedPlugin(p);
             String expected = "";
             String actual = plugin.getLicense();
