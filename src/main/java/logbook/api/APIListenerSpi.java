60f691dff1d30291673036cb3780bcb8a3cc4d02
sanaehirotaka <admin@sanaechan.net>
2015-12-14 11:04:02 +0900

    コメントを追加

--- src/main/java/logbook/api/APIListenerSpi.java
+++ src/main/java/logbook/api/APIListenerSpi.java
@@ -5,8 +5,25 @@ import javax.json.JsonObject;
 import logbook.proxy.RequestMetaData;
 import logbook.proxy.ResponseMetaData;
 
+/**
+ * JSONオブジェクトを受け取るリスナーです。<br>
+ * <br>
+ * 実装クラスはServiceLoaderより取得されます。
+ * 実装クラスが呼び出されるようにするには  META-INF/services/logbook.api.APIListenerSpi に
+ * 実装クラスの完全修飾名を記述します
+ */
 public interface APIListenerSpi {
 
+    /**
+     * JSONオブジェクトを受け取ります。<br>
+     *
+     * 実装クラスに{@link API}注釈が付与されている場合、{@link API}注釈が持つURIに対してのみ実装クラスが呼び出されます。
+     * 実装クラスに{@link API}注釈が付与されていない場合、すべてのURIに対して実装クラスが呼び出されます。
+     *
+     * @param json APIのレスポンスに含まれるJSONオブジェクト
+     * @param requestMetaData リクエスト
+     * @param responseMetaData レスポンス
+     */
     void accept(JsonObject json, RequestMetaData requestMetaData, ResponseMetaData responseMetaData);
 
 }
