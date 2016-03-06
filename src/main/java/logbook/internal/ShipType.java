d550fe9901e2a458502e754501d1b552a8acb8d8
sanaehirotaka <admin@sanaechan.net>
2016-03-06 15:45:06 +0900

    艦種と装備の定数を追加

---
+++ src/main/java/logbook/internal/ShipType.java
@@ -0,0 +1,49 @@
+package logbook.internal;
+
+import logbook.bean.Stype;
+
+/**
+ * 艦種定数
+ *
+ */
+public enum ShipType {
+
+    海防艦(1),
+    駆逐艦(2),
+    軽巡洋艦(3),
+    重雷装巡洋艦(4),
+    重巡洋艦(5),
+    航空巡洋艦(6),
+    軽空母(7),
+    巡洋戦艦(8),
+    戦艦(9),
+    航空戦艦(10),
+    正規空母(11),
+    超弩級戦艦(12),
+    潜水艦(13),
+    潜水空母(14),
+    補給艦E(15),
+    水上機母艦(16),
+    揚陸艦(17),
+    装甲空母(18),
+    工作艦(19),
+    潜水母艦(20),
+    練習巡洋艦(21),
+    補給艦(22);
+
+    private final int stype;
+
+    private ShipType(int stype) {
+        this.stype = stype;
+    }
+
+    /**
+     * この定数がstypeと等しい場合trueを返します
+     *
+     * @param stype 艦種
+     * @return 艦種がこの定数と同じ場合はtrue
+     */
+    public boolean equals(Stype stype) {
+        return this.stype == stype.getId();
+    }
+}
