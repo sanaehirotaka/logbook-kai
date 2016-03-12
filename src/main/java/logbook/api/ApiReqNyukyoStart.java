ef66237a037e3f5f32642ca74957bcbfbac84dd3
sanaehirotaka <admin@sanaechan.net>
2016-03-12 20:40:42 +0900

    入渠を実装

--- src/main/java/logbook/api/ApiReqNyukyoStart.java
+++ src/main/java/logbook/api/ApiReqNyukyoStart.java
@@ -1,7 +1,11 @@
 package logbook.api;
 
+import java.util.Map;
+
 import javax.json.JsonObject;
 
+import logbook.bean.Ship;
+import logbook.bean.ShipCollection;
 import logbook.proxy.RequestMetaData;
 import logbook.proxy.ResponseMetaData;
 
@@ -14,8 +18,17 @@ public class ApiReqNyukyoStart implements APIListenerSpi {
 
     @Override
     public void accept(JsonObject json, RequestMetaData req, ResponseMetaData res) {
-        // TODO 自動生成されたメソッド・スタブ
-
+        if ("1".equals(req.getParameterMap().get("api_highspeed").get(0))) {
+            Map<Integer, Ship> map = ShipCollection.get()
+                    .getShipMap();
+            Integer shipId = Integer.valueOf(req.getParameterMap()
+                    .get("api_ship_id")
+                    .get(0));
+            Ship ship = map.get(shipId)
+                    .clone();
+            ship.setNowhp(ship.getMaxhp());
+            ship.setNdockTime(0);
+            map.put(shipId, ship);
+        }
     }
-
 }
