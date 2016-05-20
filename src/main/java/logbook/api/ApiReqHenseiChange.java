f7005a7a9cfaa306fde5ad7a8f1da74d5faf4412
sanaehirotaka <admin@sanaechan.net>
2016-05-20 19:19:07 +0900

    Fixed #16 各艦隊タブにおける艦娘の表示のバグ

--- src/main/java/logbook/api/ApiReqHenseiChange.java
+++ src/main/java/logbook/api/ApiReqHenseiChange.java
@@ -2,6 +2,7 @@ package logbook.api;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import javax.json.JsonObject;
@@ -20,19 +21,19 @@ public class ApiReqHenseiChange implements APIListenerSpi {
 
     @Override
     public void accept(JsonObject json, RequestMetaData req, ResponseMetaData res) {
+
+        Map<Integer, DeckPort> deckMap = DeckPortCollection.get()
+                .getDeckPortMap();
+
         Integer portId = Integer.valueOf(req.getParameterMap().get("api_id").get(0));
         Integer shipId = Integer.parseInt(req.getParameterMap().get("api_ship_id").get(0));
         int shipIdx = Integer.parseInt(req.getParameterMap().get("api_ship_idx").get(0));
 
-        DeckPort deckPort = DeckPortCollection.get()
-                .getDeckPortMap()
-                .get(portId)
+        DeckPort deckPort = deckMap.get(portId)
                 .clone();
         List<Integer> ships = new ArrayList<>(deckPort.getShip());
         deckPort.setShip(ships);
-        DeckPortCollection.get()
-                .getDeckPortMap()
-                .put(portId, deckPort);
+        deckMap.put(portId, deckPort);
 
         if (shipId == -1) {
             ships.remove(shipIdx);
@@ -42,20 +43,23 @@ public class ApiReqHenseiChange implements APIListenerSpi {
             ships.replaceAll(ship -> first.equals(ship) ? ship : -1);
         } else {
             Integer from = ships.get(shipIdx);
-            for (Entry<Integer, DeckPort> entry : DeckPortCollection.get().getDeckPortMap().entrySet()) {
+            for (Entry<Integer, DeckPort> entry : deckMap.entrySet()) {
                 if (entry.getValue().getShip().contains(shipId)) {
                     DeckPort port2 = entry.getValue().clone();
                     List<Integer> ships2 = new ArrayList<>(port2.getShip());
                     port2.setShip(ships2);
-                    DeckPortCollection.get()
-                            .getDeckPortMap()
-                            .put(port2.getId(), port2);
+                    deckMap.put(port2.getId(), port2);
 
-                    ships2.set(ships2.indexOf(shipId), from);
+                    if (from == -1) {
+                        ships2.removeIf(id -> id.equals(shipId));
+                        ships2.add(-1);
+                    } else {
+                        ships2.set(ships2.indexOf(shipId), from);
+                    }
                     break;
                 }
             }
-            ships.set(shipIdx, shipId);
+            deckMap.get(portId).getShip().set(shipIdx, shipId);
         }
     }
 
