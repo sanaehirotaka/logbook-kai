7a4e65ba1346fed98c9a7ffe7699895dfc3eb1be
sanaehirotaka <admin@sanaechan.net>
2016-07-05 22:42:46 +0900

    装備改修の実装

--- src/main/java/logbook/api/ApiReqKousyouRemodelSlot.java
+++ src/main/java/logbook/api/ApiReqKousyouRemodelSlot.java
@@ -1,7 +1,13 @@
 package logbook.api;
 
+import java.util.Map;
+
+import javax.json.JsonArray;
 import javax.json.JsonObject;
 
+import logbook.bean.SlotItem;
+import logbook.bean.SlotItemCollection;
+import logbook.internal.JsonHelper;
 import logbook.proxy.RequestMetaData;
 import logbook.proxy.ResponseMetaData;
 
@@ -14,8 +20,26 @@ public class ApiReqKousyouRemodelSlot implements APIListenerSpi {
 
     @Override
     public void accept(JsonObject json, RequestMetaData req, ResponseMetaData res) {
-        // TODO 自動生成されたメソッド・スタブ
+        JsonObject data = json.getJsonObject("api_data");
+        if (data != null) {
+            Map<Integer, SlotItem> itemMap = SlotItemCollection.get()
+                    .getSlotitemMap();
 
+            // 改修後装備
+            JsonObject afterSlot = data.getJsonObject("api_after_slot");
+            if (afterSlot != null) {
+                SlotItem replace = SlotItem.toSlotItem(afterSlot);
+                itemMap.put(replace.getId(), replace);
+            }
+
+            // 消費装備
+            JsonArray useSlotId = data.getJsonArray("api_use_slot_id");
+            if (useSlotId != null) {
+                for (Integer slotId : JsonHelper.toIntegerList(useSlotId)) {
+                    itemMap.remove(slotId);
+                }
+            }
+        }
     }
 
 }
