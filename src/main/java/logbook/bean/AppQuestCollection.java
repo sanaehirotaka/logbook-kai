f7c85ffd065cf038573fb10c3c51746adaf9d2a3
sanaehirotaka <admin@sanaechan.net>
2017-08-11 00:04:05 +0900

    任務一覧でNullPointerExceptionが発生する不具合を修正

--- src/main/java/logbook/bean/AppQuestCollection.java
+++ src/main/java/logbook/bean/AppQuestCollection.java
@@ -58,12 +58,14 @@ public class AppQuestCollection implements Serializable {
         this.update();
 
         val copyMap = new ConcurrentSkipListMap<>(this.quest);
-        for (Quest quest : questList.getList()) {
-            if (quest != null) {
-                copyMap.remove(quest.getNo());
+        if (questList.getList() != null) {
+            for (Quest quest : questList.getList()) {
+                if (quest != null) {
+                    copyMap.remove(quest.getNo());
 
-                if (quest.getState() == 2) {
-                    copyMap.put(quest.getNo(), AppQuest.toAppQuest(quest));
+                    if (quest.getState() == 2) {
+                        copyMap.put(quest.getNo(), AppQuest.toAppQuest(quest));
+                    }
                 }
             }
         }
