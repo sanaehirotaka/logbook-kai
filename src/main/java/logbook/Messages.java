5a84ca3198fb8c628b779a23376b250f800450bd
sanaehirotaka <admin@sanaechan.net>
2017-10-07 10:52:33 +0900

    例外処理の改善

--- src/main/java/logbook/Messages.java
+++ src/main/java/logbook/Messages.java
@@ -12,10 +12,10 @@ import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
 import java.text.MessageFormat;
 import java.util.Locale;
-import java.util.MissingResourceException;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
+import logbook.internal.LoggerHolder;
 import logbook.plugin.PluginContainer;
 
 /**
@@ -44,7 +44,8 @@ public class Messages {
     public static String getString(String key) {
         try {
             return RESOURCE_BUNDLE.getString(key);
-        } catch (MissingResourceException e) {
+        } catch (Exception e) {
+            LoggerHolder.get().warn("リソース・バンドルから値の取得に失敗しました", e);
             return '!' + key + '!';
         }
     }
@@ -85,7 +86,7 @@ public class Messages {
                 String format,
                 ClassLoader loader,
                 boolean reload)
-                        throws IOException {
+                throws IOException {
 
             String bundleName = this.toBundleName(baseName, locale);
             String resourceName = this.toResourceName(bundleName, "properties"); //$NON-NLS-1$
