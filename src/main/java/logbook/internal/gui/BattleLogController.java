f1a3f4d524560e7c8ef8174a9bdc875f1f592e4c
kPherox <admin@mail.kr-kp.com>
2018-03-29 05:29:48 +0900

    戦闘ログに更新ボタンを追加

--- src/main/java/logbook/internal/gui/BattleLogController.java
+++ src/main/java/logbook/internal/gui/BattleLogController.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
@@ -213,66 +214,77 @@ public class BattleLogController extends WindowController {
             this.dropShip.setCellValueFactory(new PropertyValueFactory<>("dropShip"));
 
             // 統計
-            // ルート要素(非表示)
-            TreeItem<BattleLogCollect> root = new TreeItem<BattleLogCollect>(new BattleLogCollect());
-            this.collect.setRoot(root);
-
-            // 集計単位がキーのマップ
-            this.logMap = BattleLogs.readSimpleLog();
-            for (Unit unit : Unit.values()) {
-                List<SimpleBattleLog> list = this.logMap.get(unit);
-
-                // 単位のルート
-                BattleLogCollect unitRootValue = BattleLogs.collect(list, null, false);
-                unitRootValue.setUnit(unit.getName());
-                unitRootValue.setCollectUnit(unit);
-
-                TreeItem<BattleLogCollect> unitRoot = new TreeItem<BattleLogCollect>(unitRootValue);
-                unitRoot.setExpanded(true);
-
-                // ボス
-                BattleLogCollect bossValue = BattleLogs.collect(list, null, true);
-                bossValue.setUnit("ボス");
-                bossValue.setCollectUnit(unit);
-                bossValue.setBoss(true);
-
-                TreeItem<BattleLogCollect> boss = new TreeItem<BattleLogCollect>(bossValue);
-                unitRoot.getChildren().add(boss);
-
-                // 海域の名前
-                List<String> areaNames = list.stream()
-                        .map(SimpleBattleLog::getArea)
-                        .distinct()
-                        .sorted(Comparator.naturalOrder())
-                        .collect(Collectors.toList());
-                for (String area : areaNames) {
-                    // 海域毎の集計
-                    BattleLogCollect areaValue = BattleLogs.collect(list, area, false);
-                    areaValue.setUnit(area);
-                    areaValue.setCollectUnit(unit);
-                    areaValue.setArea(area);
-
-                    TreeItem<BattleLogCollect> areaRoot = new TreeItem<BattleLogCollect>(areaValue);
-
-                    // 海域ボス
-                    BattleLogCollect areaBossValue = BattleLogs.collect(list, area, true);
-                    areaBossValue.setUnit("ボス");
-                    areaBossValue.setCollectUnit(unit);
-                    areaBossValue.setArea(area);
-                    areaBossValue.setBoss(true);
-
-                    TreeItem<BattleLogCollect> areaBoss = new TreeItem<BattleLogCollect>(areaBossValue);
-                    areaRoot.getChildren().add(areaBoss);
-
-                    unitRoot.getChildren().add(areaRoot);
-                }
-
-                root.getChildren().add(unitRoot);
-
-            }
+            this.setCollect();
         } catch (Exception e) {
             LoggerHolder.get().error("FXMLの初期化に失敗しました", e);
         }
+    }
+
+    private void setCollect() {
+        // ルート要素(非表示)
+        TreeItem<BattleLogCollect> root = new TreeItem<BattleLogCollect>(new BattleLogCollect());
+        this.collect.setRoot(root);
+
+        // 集計単位がキーのマップ
+        this.logMap = BattleLogs.readSimpleLog();
+        for (Unit unit : Unit.values()) {
+            List<SimpleBattleLog> list = this.logMap.get(unit);
+
+            // 単位のルート
+            BattleLogCollect unitRootValue = BattleLogs.collect(list, null, false);
+            unitRootValue.setUnit(unit.getName());
+            unitRootValue.setCollectUnit(unit);
+
+            TreeItem<BattleLogCollect> unitRoot = new TreeItem<BattleLogCollect>(unitRootValue);
+            unitRoot.setExpanded(true);
+
+            // ボス
+            BattleLogCollect bossValue = BattleLogs.collect(list, null, true);
+            bossValue.setUnit("ボス");
+            bossValue.setCollectUnit(unit);
+            bossValue.setBoss(true);
+
+            TreeItem<BattleLogCollect> boss = new TreeItem<BattleLogCollect>(bossValue);
+            unitRoot.getChildren().add(boss);
+
+            // 海域の名前
+            List<String> areaNames = list.stream()
+                    .map(SimpleBattleLog::getArea)
+                    .distinct()
+                    .sorted(Comparator.naturalOrder())
+                    .collect(Collectors.toList());
+            for (String area : areaNames) {
+                // 海域毎の集計
+                BattleLogCollect areaValue = BattleLogs.collect(list, area, false);
+                areaValue.setUnit(area);
+                areaValue.setCollectUnit(unit);
+                areaValue.setArea(area);
+
+                TreeItem<BattleLogCollect> areaRoot = new TreeItem<BattleLogCollect>(areaValue);
+
+                // 海域ボス
+                BattleLogCollect areaBossValue = BattleLogs.collect(list, area, true);
+                areaBossValue.setUnit("ボス");
+                areaBossValue.setCollectUnit(unit);
+                areaBossValue.setArea(area);
+                areaBossValue.setBoss(true);
+
+                TreeItem<BattleLogCollect> areaBoss = new TreeItem<BattleLogCollect>(areaBossValue);
+                areaRoot.getChildren().add(areaBoss);
+
+                unitRoot.getChildren().add(areaRoot);
+            }
+
+            root.getChildren().add(unitRoot);
+        }
+    }
+
+    /**
+     * ログを更新
+     */
+    @FXML
+    void reloadAction(ActionEvent event) {
+        this.setCollect();
     }
 
     /**
