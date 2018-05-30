e9d95aeee9fb1076ffc59b1710c0f8cd292a84cd
sanaehirotaka <admin@sanaechan.net>
2018-05-30 23:48:29 +0900

    疲労回復までの時間計算をなるべく正確にする

--- src/main/java/logbook/api/ApiGetMemberShipDeck.java
+++ src/main/java/logbook/api/ApiGetMemberShipDeck.java
@@ -1,10 +1,14 @@
 package logbook.api;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Map;
+import java.util.function.Predicate;
 
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 
+import logbook.bean.AppCondition;
 import logbook.bean.DeckPort;
 import logbook.bean.DeckPortCollection;
 import logbook.bean.Ship;
@@ -35,9 +39,29 @@ public class ApiGetMemberShipDeck implements APIListenerSpi {
      * @param array api_ship_data
      */
     private void apiShipData(JsonArray array) {
+        // 差し替え前
+        Map<Integer, Ship> before = ShipCollection.get()
+                .getShipMap();
+
+        // 差し替え
         Map<Integer, Ship> map = ShipCollection.get()
                 .getShipMap();
         map.putAll(JsonHelper.toMap(array, Ship::getId, Ship::toShip));
+
+        // cond値が更新されたかを検出
+        Predicate<Ship> update = beforeShip -> {
+            Ship afterShip = map.get(beforeShip.getId());
+            if (afterShip != null) {
+                return !beforeShip.getCond().equals(afterShip.getCond());
+            }
+            return false;
+        };
+        if (before.values().stream()
+                .filter(ship -> ship.getCond() <= 49)
+                .anyMatch(update)) {
+            ZonedDateTime time = ZonedDateTime.now(ZoneId.systemDefault());
+            AppCondition.get().setCondUpdateTime(time.toEpochSecond());
+        }
     }
 
     /**
--- src/main/java/logbook/api/ApiPortPort.java
+++ src/main/java/logbook/api/ApiPortPort.java
@@ -87,7 +87,7 @@ public class ApiPortPort implements APIListenerSpi {
             return false;
         };
         if (before.values().stream()
-                .filter(ship -> ship.getCond() < 49)
+                .filter(ship -> ship.getCond() <= 49)
                 .anyMatch(update)) {
             ZonedDateTime time = ZonedDateTime.now(ZoneId.systemDefault());
             AppCondition.get().setCondUpdateTime(time.toEpochSecond());
