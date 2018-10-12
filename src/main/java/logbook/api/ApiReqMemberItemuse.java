98e9feb8a26955709827ce727b6f24e3e30a339d
sanaehirotaka <admin@sanaechan.net>
2018-10-13 00:55:29 +0900

    アイテム一覧から装備を入手した場合に所有装備に反映されない

---
+++ src/main/java/logbook/api/ApiReqMemberItemuse.java
@@ -0,0 +1,52 @@
+package logbook.api;
+
+import java.util.Map;
+import java.util.Optional;
+
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonValue;
+
+import logbook.bean.SlotItem;
+import logbook.bean.SlotItemCollection;
+import logbook.proxy.RequestMetaData;
+import logbook.proxy.ResponseMetaData;
+
+/**
+ * /kcsapi/api_req_member/itemuse
+ *
+ */
+@API("/kcsapi/api_req_member/itemuse")
+public class ApiReqMemberItemuse implements APIListenerSpi {
+
+    @Override
+    public void accept(JsonObject json, RequestMetaData req, ResponseMetaData res) {
+        JsonObject data = json.getJsonObject("api_data");
+        if (data != null) {
+            Optional.ofNullable(data.getJsonArray("api_getitem"))
+                    .ifPresent(this::apiGetitem);
+        }
+    }
+
+    /**
+     * api_data.api_getitem
+     *
+     * @param array api_getitem
+     */
+    private void apiGetitem(JsonArray array) {
+        if (array != null) {
+            Map<Integer, SlotItem> map = SlotItemCollection.get()
+                    .getSlotitemMap();
+            for (JsonValue value : array) {
+                JsonObject obj = (JsonObject) value;
+                Optional.ofNullable(obj.getJsonObject("api_slotitem"))
+                        .map(SlotItem::toSlotItem)
+                        .ifPresent(item -> {
+                            item.setLevel(0);
+                            item.setLocked(false);
+                            map.put(item.getId(), item);
+                        });
+            }
+        }
+    }
+}
