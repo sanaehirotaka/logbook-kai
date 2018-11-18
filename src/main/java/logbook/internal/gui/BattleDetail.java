1124820ac5ac81bd4c76284a87414bbead0a7792
sanaehirotaka <admin@sanaechan.net>
2018-11-18 20:38:39 +0900

    経験値を表示する

--- src/main/java/logbook/internal/gui/BattleDetail.java
+++ src/main/java/logbook/internal/gui/BattleDetail.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Supplier;
 
@@ -410,9 +411,9 @@ public class BattleDetail extends WindowController {
         // 経験値
         if (this.result != null) {
             this.baseExp.setText(String.valueOf(this.result.getGetBaseExp()));
-            this.shipExp.setText(String.valueOf(this.result.getGetShipExp().stream()
-                    .mapToInt(Integer::intValue)
-                    .sum()));
+            this.shipExp.setText(String.valueOf(this.result.getGetShipExp().stream().mapToInt(Integer::intValue).sum()
+                    + Optional.ofNullable(this.result.getGetShipExpCombined())
+                            .map(v -> v.stream().mapToInt(Integer::intValue).sum()).orElse(0)));
             this.exp.setText(this.result.getGetExp()
                     + "(" + BigDecimal.valueOf(this.result.getGetExp())
                             .divide(BigDecimal.valueOf(1428.571), 3, RoundingMode.FLOOR)
