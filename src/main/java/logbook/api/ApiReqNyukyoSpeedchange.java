c93fc60fa82a5b801136c389df7ceb9f789a5530
kPherox <admin@mail.kr-kp.com>
2019-02-19 21:23:41 +0900

    入渠中の艦娘に高速修復材を使った時の処理を実装

--- src/main/java/logbook/api/ApiReqNyukyoSpeedchange.java
+++ src/main/java/logbook/api/ApiReqNyukyoSpeedchange.java
@@ -1,7 +1,15 @@
 package logbook.api;
 
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import javax.json.JsonObject;
 
+import logbook.bean.Ndock;
+import logbook.bean.NdockCollection;
+import logbook.bean.Ship;
+import logbook.bean.ShipCollection;
 import logbook.proxy.RequestMetaData;
 import logbook.proxy.ResponseMetaData;
 
@@ -14,8 +22,36 @@ public class ApiReqNyukyoSpeedchange implements APIListenerSpi {
 
     @Override
     public void accept(JsonObject json, RequestMetaData req, ResponseMetaData res) {
-        // TODO 自動生成されたメソッド・スタブ
+        Map<Integer, Ndock> ndockMap = NdockCollection.get()
+                .getNdockMap();
+        Integer ndockId = Integer.valueOf(req.getParameter("api_ndock_id"));
+        Ndock ndock = ndockMap.get(ndockId);
 
+        Map<Integer, Ship> shipMap = ShipCollection.get()
+                .getShipMap();
+        Integer shipId = ndock.getShipId();
+        Ship ship = shipMap.get(shipId).clone();
+
+        // 艦娘を修理完了状態にする
+        ship.setNowhp(ship.getMaxhp());
+        ship.setNdockTime(0);
+
+        shipMap.put(shipId, ship);
+
+        // 高速修復材を使った入渠ドックを初期化
+        ndock.setCompleteTime(0L);
+        ndock.setShipId(-1);
+        ndock.setState(0);
+
+        ndockMap.put(ndockId, ndock);
+
+        // 入渠中の艦娘
+        NdockCollection.get()
+                .setNdockSet(ndockMap.entrySet()
+                        .stream()
+                        .map(Map.Entry::getValue)
+                        .map(Ndock::getShipId)
+                        .collect(Collectors.toCollection(LinkedHashSet::new)));
     }
 
 }
